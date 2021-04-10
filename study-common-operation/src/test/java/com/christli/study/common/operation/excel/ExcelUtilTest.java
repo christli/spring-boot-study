ae525c7b7cb761369f749d9d728532604fc4acda
christli <christli@meijiabang.cn>
2021-04-10 20:43:49 +0800

    操作Excel

---
+++ study-common-operation/src/test/java/com/christli/study/common/operation/excel/ExcelUtilTest.java
@@ -0,0 +1,34 @@
+package com.christli.study.common.operation.excel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//用的时候，再打开CreateNewWorkbook等注释
+@SpringBootTest
+class ExcelUtilTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void TestCreateWorkbook() {
+//        ExcelUtil.CreateNewWorkbook();
+    }
+
+    @Test
+    public void TestCreateSheet() {
+//        ExcelUtil.CreateNewSheet();
+    }
+
+    @Test
+    public void TestCreateCell() {
+//        ExcelUtil.CreateNewCell();
+    }
+
+    @Test
+    public void OpenAndReadExcel() {
+//        ExcelUtil.OpenExcelByFile();
+//        ExcelUtil.OpenExcelByFileInputStream();
+    }
+}
